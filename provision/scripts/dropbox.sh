c99ed90b9c17fbb53da6855d969ab529110515a0
Dmitriy Tyurin <fobia3d@gmail.com>
2014-06-15 09:04:36 +0400

    add ssh bin

--- provision/scripts/dropbox.sh
+++ provision/scripts/dropbox.sh
@@ -2,12 +2,26 @@
 
 echo ">>> Installing Dropbox cloud"
 
+DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
+
+
+if [[ "$(id -u)" == "0" ]]; then
+    echo "!!!  You is root"
+    exit 1
+fi
+
+cd ~
+
+
 # Установка программы Dropbox из командной строки
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86" | tar xzf -
 ~/.dropbox-dist/dropboxd
 
-if  [ -f /usr/local/bin/dropbox ]; then
+if  [[ ! -f ~/dropbox.py ]]; then
     wget -O dropbox.py https://www.dropbox.com/download?dl=packages/dropbox.py
     chmod +x dropbox.py
+fi
+
+if [[ "$(id -u)" == "0" ]]; then
     mv dropbox.py /usr/local/bin/dropbox
 fi
