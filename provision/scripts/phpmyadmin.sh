d9475e79fdcafd096bbe48ac48c2fd311e7f0910
Dmitriy Tyurin <fobia3d@gmail.com>
2014-07-28 11:01:38 +0400

    Конфиги phpmyadmin

--- provision/scripts/phpmyadmin.sh
+++ provision/scripts/phpmyadmin.sh
@@ -6,6 +6,11 @@ mkdir -p /usr/share/htdocs
 apt-get install -y zip
 
 cd /usr/share/htdocs
+if [ -d /usr/share/htdocs/phpmyadmin  ]; then
+    echo ">>> ==> phpmyadmin is installed"
+    exit
+fi
+
 echo ">>> ==> download (... phpmyadmin)"
 wget --no-check-certificate http://github.com/fobia/srv-tools-phpmyadmin/archive/master.zip
 unzip master.zip
@@ -13,7 +18,8 @@ unzip master.zip
 mv srv-tools-phpmyadmin-master phpmyadmin
 rm master.zip
 
-cat > /etc/apache2/conf.d/phpmyadmin.conf <<EOF
+CONF_FILE="/etc/apache2/conf.d/20-phpmyadmin.conf"
+cat > "$CONF_FILE" <<EOF
 Alias /phpmyadmin/ /usr/share/htdocs/phpmyadmin/
 <Location /usr/share/htdocs/phpmyadmin/>
     # AllowOverride All
