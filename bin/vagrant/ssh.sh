a1519d02e14205f2b60559ac26689bb6a1126420
Dmitriy Tyurin <fobia3d@gmail.com>
2014-09-10 02:05:27 +0400

    add auto up vagrant

--- bin/vagrant/ssh.sh
+++ bin/vagrant/ssh.sh
@@ -3,4 +3,15 @@ set -e
 
 DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 cd "$DIR/../.."
+
+## $ Current machine states:
+## $
+## $ default                   aborted (virtualbox)
+## $ default                   running (virtualbox)
+MSG=$(vagrant status | sed -n '/running (virtualbox)/p')
+if [[ "$MSG" == "" ]]; then
+    vagrant up
+fi
+##
+
 vagrant ssh $*
