39859f6d2c517ed2b5b75fe5a0bd59bcba7faea8
Dmitriy Tyurin <fobia3d@gmail.com>
2015-07-01 13:05:49 +0000

    update nodejs

--- provision/scripts/libs/nodejs.sh
+++ provision/scripts/libs/nodejs.sh
@@ -5,28 +5,27 @@ echo ">>> Installing nodejs"
 
 NODE_URL_86="http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x86.tar.gz"
 NODE_URL_64="http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x64.tar.gz"
+NODE_LATEST="http://nodejs.org/dist/latest/"
 
 
-if [ "$(uname -m 2>/dev/null | grep 64)" != "" ]; then
-    NODE_URL="$NODE_URL_64"
-    echo ">>> ==> machine x64 ..."
-else
-    NODE_URL="$NODE_URL_86"
-    echo ">>> ==> machine x86 ..."
-fi
+VM="86"
+if [ "$(uname -m 2>/dev/null | grep 64)" != "" ]; then VM="64"; fi
 
+LATEST=$(curl -sS http://nodejs.org/dist/latest/SHASUMS.txt | grep -o -P 'node-.+linux-x.+\.tar\.gz' | grep "$VM")
+NODE_URL="http://nodejs.org/dist/latest/${LATEST}"
 
 mkdir -p /usr/local/lib
 cd /usr/local/lib
 
 if [ "$(node -v 2>/dev/null | grep 'v0')" = "" ]; then
-    echo ">>> ==> download (... node-v0.10.26-linux)"
+    echo ">>> ==> download (... ${LATEST})"
+    exit
     wget --no-check-certificate -q -O - "$NODE_URL" | tar -xzf -
     ln -s /usr/local/lib/node*/bin/node /usr/local/bin/node
     ln -s /usr/local/lib/node*/bin/npm /usr/local/bin/npm
 
     echo ">>> ==> npm install (less, http-console, jsmin)"
     npm install -g less http-console jsmin
-fi
 
-rm -r ~/.npm
+    rm -rf ~/.npm
+fi
--- provision/scripts/php/install.sh
+++ provision/scripts/php/install.sh
@@ -5,12 +5,17 @@ DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 TMP_PWD_DIR=$(pwd)
 
 cd $DIR
+####################################################
+
 ./php53.sh
+# VERSION=$(php -v | grep -o -P '(?>PHP )(5[^ ]+) ')
+# VERSION=${VERSION:3:-1}
 
 echo ">>> Installing PHP"
 
 apt-get install -y php5  php5-mysql php5-cli php5-mcrypt \
-                   php5-gd php5-xdebug php5-curl php5-dev  php5-xsl
+                   php5-gd php5-xdebug php5-curl php5-dev  php5-xsl \
+                   php5-imap
 
 
 ./config.sh
